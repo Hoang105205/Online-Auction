07f1bb28734f1469f14a1d1271efcf477dde42a4
Ruby <nhthuan23@clc.fitus.edu.vn>
2025-11-11 14:47:58 +0700

    feat: DetailsPage demo.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,25 @@
-import { Route, createBrowserRouter, RouterProvider, createRoutesFromElements } from 'react-router-dom';
+import {
+  Route,
+  createBrowserRouter,
+  RouterProvider,
+  createRoutesFromElements,
+} from "react-router-dom";
 
-import MainLayout from './layouts/MainLayout';
-import LandingPage from './pages/LandingPage';
-
+import MainLayout from "./layouts/MainLayout";
+import LandingPage from "./pages/LandingPage";
+import DetailsPage from "./pages/DetailsPage";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<MainLayout />}>
       <Route index element={<LandingPage />} />
-
+      <Route path="details/:id" element={<DetailsPage />} />
     </Route>
   )
 );
 
 function App() {
-  return (
-    <RouterProvider router={router} />
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
