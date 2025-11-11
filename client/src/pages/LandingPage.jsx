ac349197736dd50a3589b0f0316f56c06fedc872
Pipi1225 <dghuy6625@gmail.com>
2025-11-11 20:46:09 +0700

    feat: Landing page demo

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -1,10 +1,11 @@
 import Header from "../components/Header";
-
+import LandingProduct from "../components/LandingProduct";
 
 const LandingPage = () => {
   return (
     <>
         <Header />
+        <LandingProduct />
     </>
   )
 }
