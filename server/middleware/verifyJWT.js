6a52cd48ba391d2e813c05ee0e0cb8cd172c8148
Hoang105205 <huyhoangluu105@gmail.com>
2025-11-30 16:31:21 +0700

    update: respone when verify JWT

--- server/middleware/verifyJWT.js
+++ server/middleware/verifyJWT.js
@@ -11,7 +11,7 @@ const verifyJWT = (req, res, next) => {
 
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
     if (err) {
-      return res.sendStatus(403); // Forbidden
+      return res.sendStatus(401); // Unauthorized
     }
 
     req.user = decoded.UserInfo.id;
