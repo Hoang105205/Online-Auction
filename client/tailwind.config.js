72320bf3eea6fa0f0320a2389c1ad9fa58abe773
Pipi1225 <dghuy6625@gmail.com>
2025-12-05 16:38:51 +0700

    update: pulsing effect and red button for products created within the first <N> minutes

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -6,10 +6,26 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    ".flowbite-react\\class-list.json"
+    ".flowbite-react\\class-list.json",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        "pulse-border": "pulse-border 2s cubic-bezier(0.4, 0, 0.6, 1) infinite",
+      },
+      keyframes: {
+        "pulse-border": {
+          "0%, 100%": {
+            boxShadow:
+              "0 0 0 0 rgba(250, 204, 21, 0.7), 0 10px 15px -3px rgba(250, 204, 21, 0.3)",
+          },
+          "50%": {
+            boxShadow:
+              "0 0 0 8px rgba(250, 204, 21, 0), 0 20px 25px -5px rgba(250, 204, 21, 0.2)",
+          },
+        },
+      },
+    },
   },
   plugins: [flowbiteReact],
-}
+};
